97dc8715f23e39be1d0ce3f3eac077f5d2eec50e
ebuka <peterdbrainy5@gmail.com>
2022-06-20 17:02:26 +0100

    Set up the redux architecture, set up some boilerplate code for auth types, auth-action and auth-reducer. Install redux-logger, redux-persist, redux-thunk and react-toastify dependency

---
+++ redux/apiconfig/axiosconfig.js
@@ -0,0 +1,3 @@
+const PATH = 'https://ellevate.com/';
+
+export { PATH };
---
+++ redux/store.js
@@ -0,0 +1,17 @@
+// import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+// import thunkMiddleware from 'redux-thunk';
+// import { createLogger } from 'redux-logger';
+// import rootReducer from './reducers/rootReducer';
+// import { persistStore } from 'redux-persist';
+
+// const loggerMiddleware = createLogger();
+
+// export const store = createStore (
+//     rootReducer,
+//     applyMiddleware(
+//         thunkMiddleware,
+//         loggerMiddleware
+//     )
+//     );
+
+// export const persistor = persistStore(store);
