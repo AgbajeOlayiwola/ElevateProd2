bd37589107968cdcdb02f5b38551c0bc08ad7bc1
agbajeolayiwola <agbajeolaiwola@gmail.com>
2022-07-04 19:57:07 +0100

    'form-flow'

--- components/ReusableComponents/ShomeSvg/index.js
+++ components/ReusableComponents/ShomeSvg/index.js
@@ -15,23 +15,23 @@ const SideBarHomeSvg = ({ fillColor }) => {
                     <path
                         d="M3.26409 10.553H1.29297L10.163 1.52539L19.033 10.553H17.0619"
                         stroke="#407A00"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                     />
                     <path
                         d="M3.06641 10.5527V17.5742C3.06641 18.1062 3.27408 18.6165 3.64373 18.9927C4.01339 19.369 4.51475 19.5803 5.03753 19.5803H14.8931C15.4159 19.5803 15.9173 19.369 16.2869 18.9927C16.6566 18.6165 16.8642 18.1062 16.8642 17.5742V10.5527"
                         stroke="#407A00"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                     />
                     <path
                         d="M12.329 10.5527H8.38672V14.565H12.329V10.5527Z"
                         stroke="#407A00"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                     />
                 </svg>
             ) : (
@@ -45,23 +45,23 @@ const SideBarHomeSvg = ({ fillColor }) => {
                     <path
                         d="M3.26409 10.553H1.29297L10.163 1.52539L19.033 10.553H17.0619"
                         stroke="#CCCCCC"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
                         stroke-linejoin="round"
                     />
                     <path
                         d="M3.06641 10.5527V17.5742C3.06641 18.1062 3.27408 18.6165 3.64373 18.9927C4.01339 19.369 4.51475 19.5803 5.03753 19.5803H14.8931C15.4159 19.5803 15.9173 19.369 16.2869 18.9927C16.6566 18.6165 16.8642 18.1062 16.8642 17.5742V10.5527"
                         stroke="#CCCCCC"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                     />
                     <path
                         d="M12.329 10.5527H8.38672V14.565H12.329V10.5527Z"
                         stroke="#CCCCCC"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                     />
                 </svg>
             )}
