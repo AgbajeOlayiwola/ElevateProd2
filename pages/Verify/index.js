bb40a8d2f40673aa8ac0edcbac192a90cb94784a
Aibinuomo Ifemide Babatope <aib.ifemide@gmail.com>
2022-08-02 03:29:08 +0100

    email_flow

--- pages/Verify/index.js
+++ pages/Verify/index.js
@@ -5,31 +5,32 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 import Failed from './Fail';
 const Verify = () => {
-    const router = useRouter();
+    // const router = useRouter();
     const [activeBtn, setActiveBtn] = useState(true);
     const [res, setRes] = useState('');
-    const token = router.query['token'];
 
-    // console.log(token);
     const handleClick = () => {
         router.push('./Auth/Login');
     };
+    const { query, isReady } = useRouter();
     useEffect(() => {
-        axios
-            .get(
-                `http://ellevate-app.herokuapp.com/verification/email/${token}`
-            )
-            .then((response) => {
-                console.log(
-                    'complete business profiler',
-                    response.data.message
-                );
-                setRes(response.data.message);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+        var token = query['token'];
+        console.log('hello', token);
+        if (!isReady) return;
+        if (token) {
+            axios
+                .get(
+                    `http://ellevate-app.herokuapp.com/verification/email/${token}`
+                )
+                .then((response) => {
+                    console.log(response.data.message);
+                    setRes(response.data.message);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, [isReady]);
 
     return (
         <>
