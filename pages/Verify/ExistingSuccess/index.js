44b6f3c40f1ce8b97ad5f2fe81cbfc917f716826
Aibinuomo Ifemide Babatope <aib.ifemide@gmail.com>
2022-09-06 11:56:18 +0100

    Update_12:090

--- pages/Verify/ExistingSuccess/index.js
+++ pages/Verify/ExistingSuccess/index.js
@@ -6,6 +6,7 @@ import Success from '../../../components/ReusableComponents/Success';
 import { ButtonComp } from '../../../components';
 
 const ExistingSuccess = ({ handleShowSuccessStep }) => {
+    const [activeBtn, setActiveBtn] = useState(true);
     let accountDetails;
     let accountNumber;
     if (typeof window !== 'undefined') {
