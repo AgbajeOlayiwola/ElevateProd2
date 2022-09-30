fbc1babd5716a5098fa022797c8b6e3f1185846c
Aibinuomo Ifemide Babatope <aib.ifemide@gmail.com>
2022-09-30 14:37:41 +0100

    stageForMandateOnWay

--- redux/actions/actions.js
+++ redux/actions/actions.js
@@ -1156,6 +1156,7 @@ export const createBusProfileSetup = (businessProfileData) => {
                     axiosInstance
                         .post(
                             `https://ellevate-test.herokuapp.com${apiRoutes.verifyStatusBus}`,
+                            [],
 
                             {
                                 headers: {
