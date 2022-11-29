6e9a8b0de7798700c5f41845ba39004aaa9879a9
Aibinuomo Ifemide Babatope <aib.ifemide@gmail.com>
2022-11-29 09:35:03 +0100

    09:34

--- pages/AccountUpgrade/index.js
+++ pages/AccountUpgrade/index.js
@@ -825,7 +825,7 @@ const AccountUpgrade = () => {
                                                                   >
                                                                       {
                                                                           item.lgaName
-                                                                      }
+                                          n                           }
                                                                   </option>
                                                               );
                                                           }
