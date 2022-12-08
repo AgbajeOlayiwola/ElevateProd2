090c1f191015f1ee3d5974b37422fc5edc0bd70f
Aibinuomo Ifemide Babatope <aib.ifemide@gmail.com>
2022-12-08 13:05:36 +0100

    newRepo_13:05am

--- pages/Verify/Account/loading.js
+++ pages/Verify/Account/loading.js
@@ -30,7 +30,84 @@ const AccountLoading = () => {
 
     const { newAccount, newAccountErrorMessage } = useSelector(
         (state) => state.newUserAccountDets
-    );
+    ); // We need ref in this, because we are dealing
+    // with JS setInterval to keep track of it and
+    // stop it when needed
+    const Ref = useRef(null);
+
+    const [timer, setTimer] = useState('00:00:60');
+
+    const getTimeRemaining = (e) => {
+        const total = Date.parse(e) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+        return {
+            total,
+            hours,
+            minutes,
+            seconds
+        };
+    };
+
+    const startTimer = (e) => {
+        let { total, hours, minutes, seconds } = getTimeRemaining(e);
+        if (total >= 0) {
+            // update the timer
+            // check if less than 10 then we need to
+            // add '0' at the beginning of the variable
+            setTimer(
+                (hours > 9 ? hours : '0' + hours) +
+                    ':' +
+                    (minutes > 9 ? minutes : '0' + minutes) +
+                    ':' +
+                    (seconds > 9 ? seconds : '0' + seconds)
+            );
+        }
+    };
+
+    const clearTimer = (e) => {
+        // If you adjust it you should also need to
+        // adjust the Endtime formula we are about
+        // to code next
+        setTimer('00:00:60');
+
+        // If you try to remove this line the
+        // updating of timer Variable will be
+        // after 1000ms or 1sec
+        if (Ref.current) clearInterval(Ref.current);
+        const id = setInterval(() => {
+            startTimer(e);
+        }, 1000);
+        Ref.current = id;
+    };
+
+    const getDeadTime = () => {
+        let deadline = new Date();
+
+        // This is where you need to adjust if
+        // you entend to add more time
+        deadline.setSeconds(deadline.getSeconds() + 50);
+        return deadline;
+    };
+
+    // We can use useEffect so that when the component
+    // mount the timer will start as soon as possible
+
+    // We put empty array to act as componentDid
+    // mount only
+    useEffect(() => {
+        clearTimer(getDeadTime());
+    }, []);
+
+    // Another way to call the clearTimer() to start
+    // the countdown is via action event from the
+    // button first we create function to be called
+    // by the button
+    // const onClickReset = () => {
+    //     clearTimer(getDeadTime());
+    // };
+
     const [count, setCount] = useState(0);
     const newUserAccountt = () => {
         const accountData = {
@@ -69,16 +146,20 @@ const AccountLoading = () => {
             //console.log(accountStatus.messages, errorMessages);
             router.push('/Succes');
         }
+        if (timer === '00:00:00') {
+            router.push('/Auth/Login');
+        }
 
         // setTimeout(() => {
         //     setError(
         //         'Your account creatio is taking a while, once its completed an email will be sent to you'
         //     );
         // }, 60000);
-    }, [errorMessages, accountStatuss]);
+    }, [errorMessages, accountStatuss, timer]);
 
     return (
         <>
+            {/* <h2>{timer}</h2> */}
             <div className={styles.cover}>
                 <div className={styles.covInn}>
                     <div className={styles.load}>
