26d2dd8c010647bd484dcc335d61b969ccd509c4
Rasheed Iskilu <riskilu@plateaumed.com>
2023-12-22 13:10:53 +0100

    feature-einvoice-ewallet

--- components/Invoices/CreateInvoice/Product.jsx
+++ components/Invoices/CreateInvoice/Product.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import IconSearch from '../../ReusableComponents/IconComponents/IconSearch';
 import { FlexBadgeContainer } from '../InvoicesStyle';
-
+import CustomDropdown from '../../CustomSelect';
+const options = ['Item 1', 'Item 2', 'Item 3'];
 const Product = () => {
   return (
     <>
@@ -15,28 +16,9 @@ const Product = () => {
       </InputWrapper>
       <FlexBadgeContainer style={ { marginTop: '20px' } }>
         <section>
-          { ' ' }
-          <select id="selectElement">
-            <option
-              value=""
-              selected
-              disabled
-              style={ {
-                color: '#A5A5A5',
-                fontWeight: 400,
-                fontSize: '14px'
-              } }
-            >
-              Select items
-            </option>
-            <option value="option1" defaultValue={ 'All items' }>All items</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
-            <option value="option4">Option 4</option>
-          </select>
+          <CustomDropdown options={ options } />
         </section>
         <section>
-          { ' ' }
           <select id="selectElement">
             <option
               value=""
