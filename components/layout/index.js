a7e3e325a7b604293be15cde591de9360f4c6d9d
Agbajeolayiwola <agbajeolaiwola@gmail.com>
2024-01-13 05:16:12 +0100

    WIP

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Footer from './footer';
-import Navbar from './navbar';
 
 const Layout = ({ children }) => {
     return (
---
+++ pages/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar } from '../../components';
+
+const About = () => {
+    const me = ['ola', 'new'];
+
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    );
+};
+
+export default About;
